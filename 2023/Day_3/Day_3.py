43938272e0a3e2c9c1383c702009886d8e0898c9
Donald Kaulukukui <donald.kaulukukui@arl.hawaii.edu>
2023-12-08 17:02:02 -1000

    not working, answer too low

--- 2023/Day_3/Day_3.py
+++ 2023/Day_3/Day_3.py
@@ -39,6 +39,8 @@ def main(part):
 
         line_counter = 0
 
+        special_characters = "!@#$%^&*()-+?_=,<>/\\"
+
         #iterate through prepared data, put everythin into a 2D array 
         for line in data:
             #do something for each line     
@@ -49,53 +51,85 @@ def main(part):
 
         number_list = []
         number_locations = []
+        part_numbers = []
 
         ##############################################Part 1 ############################################
 
         for y in range(len(part_number_matrix)):  #for every row
             for x in range(len(part_number_matrix[0])): #check every character
-                if (part_number_matrix[y][x].isnumeric()):  #if it is a number
-                    number_list.append(part_number_matrix[y][x]) #add it to the number list, top left = 0,0,  y,x
-                else:                       
+
+                char = part_number_matrix[y][x]
+
+                if (char.isnumeric()):  #if it is a number
+                    number_list.append(char) #add it to the number list, top left = 0,0,  y,x
+                else:
+                    if (char not in special_characters)  and  (char != '.'):
+                        print(char)                      
+
                     if len(number_list)>0:                  #if number list isnt empty
                         #do something with the number
-                        print(number_list)
+                        #print(number_list)
+
+                        number_string = ""
+                        for c in number_list:
+                            number_string  = number_string + c
+
+                        #print(number_string)
+
                         #check every character around the number for a symbol
                         #build list of places to check
                         #progamming logic means that we would only get here with a non-empty number list 
                         #after we've reached a non-number 
                         # so first char location =  current row, current column - length of number list
                         number_length = len(number_list)
-                        first_char_location = [y, x-number_length]
-                        last_char_location = [y, x-1]
+                        first_char_location = x-number_length
+                        last_char_location = x-1
+
+                        #print("number len is " + str(number_length))
+                        #print("number list row = " + str(y))
+                        #print("first char location = " + str(first_char_location))
+                        #print("last char location = " + str(last_char_location))
+
+                        #check all characters surrounding the number_list
+
+                        #check the row above and below
+                        #if( y == 0):
+                        #    print("checking rows = " + str(y-1) + " to " + str(y+1))
+                        #    print("checking columns = " + str(first_char_location-1) + " to " +  str(last_char_location+1))
+
+                        for y_to_check in range(y-1,y+2):
+
+                            if (y_to_check  >= 0) and (y_to_check <= line_counter-1):
+
+                                for x_to_check in range(first_char_location-1,last_char_location+2):
+
+                                    if part_number_matrix[y_to_check][x_to_check] in special_characters:
+                                        #print("Special Char found at row  " +str(y_to_check) + ", column  " + str(x_to_check) + " it is " + part_number_matrix[y_to_check][x_to_check])
+                                        part_numbers.append(int(number_string))
+                            #else:
+                            #    print("exceeded max rows or went too low")
 
 
-                        for index, i in enumerate(number_list):
-                            number_locations.append([y,x-(1+index)])
-
-                        print("number len is " + str(number_length))
-                        print("first char location = " + str(first_char_location))
-                        print("last char location = " + str(last_char_location))
-                        print(number_locations)
-
-                        #check first
-                        #
-                        #
-                        #
                         #if there is a symbol then save it as a part number
+
+
+
+
                     number_list = []                        #reset the number list
                     number_locations = []                   #reset the number locations
 
-
+                
         ###################################################################################################
         if part == 1:
 
-            print(part_number_matrix[0])
+            
+            print(part_numbers)
+            print(special_characters)
    
-            return 1
+            return sum(part_numbers)
         else:
         
             return 2
 if __name__ == "__main__":
     print(main(1))
-    print(main(2))
+    #print(main(2))
