2a82c0e71556e879f6313fb4103fa8d19544b29c
Donald Kaulukukui <donald.kaulukukui@arl.hawaii.edu>
2023-12-11 17:16:25 -1000

    not working,  part 2 in progress

--- 2023/Day_3/Day_3.py
+++ 2023/Day_3/Day_3.py
@@ -18,7 +18,7 @@ elif platform == "win32":
 
     mydir = "C:\\Users\\donal\\Documents\\Projects\\Advent_Of_Code\\Advent_Of_Code\\" + str(year) + "\\Day_" + str(day)
 
-myfile = "input.txt"    
+myfile = "sample.txt"    
 full_file_path_name = os.path.join(mydir, myfile)
 print(full_file_path_name)
 
@@ -51,108 +51,154 @@ def main(part):
         ## input data is now in a 2D array
 
         number_list = []
+        number_list_2=[]
         number_locations = []
         part_numbers = []
         length_of_row = len(part_number_matrix[0])
 
 
-        ##############################################Part 1 ############################################
-
-        for y in range(len(part_number_matrix)):  #for every, row 0-139
-            for x in range(length_of_row): #check every character in each column 0-139
-
-                char = part_number_matrix[y][x]
-
-                if (char.isnumeric()):  #if it is a number
-                    number_list.append(char) #add it to the number list, top left = 0,0,  y,x
-
-                else:
-                    #if (char not in special_characters)  and  (char != '.'):
-                    #    print(char)                      
-
-                    if len(number_list)>0:                  #if number list isnt empty
-                        #do something with the number
-                        #print(number_list)
-
-
-                        #turn number list into a string
-                        number_string = ""
-                        for c in number_list:
-                            number_string  = number_string + c
-
-                        #print(number_string)
-
-                        #check every character around the number for a symbol
-                        #build list of places to check
-                        #progamming logic means that we would only get here with a non-empty number list 
-                        #after we've reached a non-number 
-                        # so first char location =  current row, current column - length of number list
-                        number_length = len(number_list)
-                        
-                        if (x == 0): #edge case where part number ends at the end of a line
-                        
-                            first_char_location = length_of_row - number_length
-                            last_char_location = length_of_row - 1
-                        
-                            #print("row overflow condition")
-                            #print("checking rows = " + str(y-1) + " to " + str(y+1))
-                            #print("checking columns = " + str(first_char_location-1) + " to " +  str(last_char_location))
-                        else:
-                            first_char_location = x-number_length
-                            last_char_location = x-1
-
-                        #print("number len is " + str(number_length))
-                        #print("number list row = " + str(y))
-                        #print("first char location = " + str(first_char_location))
-                        #print("last char location = " + str(last_char_location))
-
-                        #check all characters surrounding the number_list
-
-                        #check the row above and below
-                        if( number_string == "324"):
-                            print("found 324 condition")
-                            print("checking rows = " + str(y-1) + " to " + str(y+1))
-                            print("checking columns = " + str(first_char_location-1) + " to " +  str(last_char_location))
-
-                        for y_to_check in range(y-1,y+2): 
-
-                            if (x==0):
-                                y_to_check =  y_to_check - 1
-
-                            if (y_to_check  >= 0) and (y_to_check <= line_counter-1):   # only check rows within the bounds of the matrix
-
-                                for x_to_check in range(first_char_location-1,last_char_location+2):
-
-                                    if x_to_check > length_of_row-1:   # dont  let x exceed # of columns
-                                        x_to_check  =  length_of_row-1
-
-                                    #if there is a symbol then save  part number
-                                    if( number_string == "324"):
-                                        print("checking char " + part_number_matrix[y_to_check][x_to_check] + " at y=" + str(y_to_check) + " x=" + str(x_to_check))
-
-
-                                    if part_number_matrix[y_to_check][x_to_check] in special_characters:
-                                        #print("Special Char found at row  " +str(y_to_check) + ", column  " + str(x_to_check) + " it is " + part_number_matrix[y_to_check][x_to_check])
-
-                                        if( number_string == "324"):
-                                            print("Special Char found at row  " +str(y_to_check) + ", column  " + str(x_to_check) + " it is " + part_number_matrix[y_to_check][x_to_check])
-
-                                            print("found 324 condition")
-                                            print("appending part number = " + number_string)
-
-                                        part_numbers.append(int(number_string))
-
-                    number_list = []                        #reset the number list
-                    number_locations = []                   #reset the number locations
-
-                
-        ###################################################################################################
         if part == 1:       
-            #print(part_numbers)
+            ##############################################Part 1 ############################################
+
+            for y in range(len(part_number_matrix)):  #for every, row 0-139
+                for x in range(length_of_row): #check every character in each column 0-139
+
+                    char = part_number_matrix[y][x]
+
+                    if (char.isnumeric()):  #if it is a number
+                        number_list.append(char) #add it to the number list, top left = 0,0,  y,x
+
+                    else:
+                        #if (char not in special_characters)  and  (char != '.'):
+                        #    print(char)                      
+
+                        if len(number_list)>0:                  #if number list isnt empty
+                            #do something with the number
+                            #print(number_list)
+
+
+                            #turn number list into a string
+                            number_string = ""
+                            for c in number_list:
+                                number_string  = number_string + c
+
+                            #print(number_string)
+
+                            #check every character around the number for a symbol
+                            #build list of places to check
+                            #progamming logic means that we would only get here with a non-empty number list 
+                            #after we've reached a non-number 
+                            # so first char location =  current row, current column - length of number list
+                            number_length = len(number_list)
+                            
+                            if (x == 0): #edge case where part number ends at the end of a line
+                            
+                                first_char_location = length_of_row - number_length
+                                last_char_location = length_of_row - 1
+                            
+                                #print("row overflow condition")
+                                #print("checking rows = " + str(y-1) + " to " + str(y+1))
+                                #print("checking columns = " + str(first_char_location-1) + " to " +  str(last_char_location))
+                            else:
+                                first_char_location = x-number_length
+                                last_char_location = x-1
+
+                            #print("number len is " + str(number_length))
+                            #print("number list row = " + str(y))
+                            #print("first char location = " + str(first_char_location))
+                            #print("last char location = " + str(last_char_location))
+
+                            #check all characters surrounding the number_list
+
+                            #check the row above and below
+
+                            for y_to_check in range(y-1,y+2): 
+
+                                if (x==0):
+                                    y_to_check =  y_to_check - 1
+
+                                if (y_to_check  >= 0) and (y_to_check <= line_counter-1):   # only check rows within the bounds of the matrix
+
+                                    for x_to_check in range(first_char_location-1,last_char_location+2):
+
+                                        if x_to_check > length_of_row-1:   # dont  let x exceed # of columns
+                                            x_to_check  =  length_of_row-1
+
+                                        if part_number_matrix[y_to_check][x_to_check] in special_characters:
+                                            #print("Special Char found at row  " +str(y_to_check) + ", column  " + str(x_to_check) + " it is " + part_number_matrix[y_to_check][x_to_check])
+                                            part_numbers.append(int(number_string))
+
+                        number_list = []                        #reset the number list
+                        number_locations = []                   #reset the number locations
+
+                    
+            ###################################################################################################
             return sum(part_numbers)
         else:
+            ##############################################Part 2 ############################################
+
+            touches = False
+
+            for y in range(len(part_number_matrix)):  #for every, row 0-139
+                for x in range(length_of_row): #check every character in each column 0-139
+
+                    char = part_number_matrix[y][x]
+
+                    if (char == '*'):  #if char is a *
+
+                        print("found a potential gear  at row " +  str(y) + " column " + str(x))
+
+                        #check surrounding area for gear ratios
+
+                        area_to_check_col_start = x  - 3
+                        area_to_check_col_end = x + 3
+
+                        area_to_check_row_start = y - 1
+                        area_to_check_row_end  = y + 1
+
+                        #check limits and adjust if necessarry
+
+                        if x < 0:  area_to_check_col_start = 0
+                        elif x > length_of_row:  area_to_check_col_end = length_of_row
+
+                        if y < 0: area_to_check_row_start  = 0
+                        elif y > line_counter: area_to_check_row_end = line_counter
+
+                        print("Checking rows" + str(area_to_check_row_start) + "-" + str(area_to_check_row_end))
+                        print("Checking columns "+ str(area_to_check_col_start) + "-" + str(area_to_check_col_end))
+
+                        for y_to_check in range(area_to_check_row_start,area_to_check_row_end+1):
+                            for x_to_check in range(area_to_check_col_start,area_to_check_col_end+1):
+
+                                char = part_number_matrix[y_to_check][x_to_check]
+
+                                if (char.isnumeric()):  #if it is a number 
+                                    number_list_2.append(char) #add it to the number list, top left = 0,0,  y,x
+                                else: #
+                                    if len(number_list_2)>0:                  #if number list isnt empty
+                                        #do something with the number
+
+                                        #check to see if it touches the asterick
+                                        
+
+                                        print(number_list_2)
+
+
+                                        #rest list buffer and touches check
+                                        number_list_2 = []
+                                        touches = False 
+
+
+
+                    #else:
+
+                    #    break
+ 
+                    
+            ###################################################################################################
+
         
             return 2
 if __name__ == "__main__":
     print(main(1))
-    #print(main(2))
+    print(main(2))
