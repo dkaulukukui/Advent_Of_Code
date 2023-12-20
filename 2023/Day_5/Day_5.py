75fc370a3f0f3bbc6d91384505ed1daa1c75bcd3
Donald Kaulukukui <donald.kaulukukui@arl.hawaii.edu>
2023-12-19 14:26:52 -1000

    day 5 part 1 complete

--- 2023/Day_5/Day_5.py
+++ 2023/Day_5/Day_5.py
@@ -18,8 +18,8 @@ elif platform == "win32":
 
     mydir = "C:\\Users\\donal\\Documents\\Projects\\Advent_Of_Code\\Advent_Of_Code\\" + str(year) + "\\Day_" + str(day)
 
-#myfile = "input.txt" 
-myfile = "sample.txt"   
+myfile = "input.txt" 
+#myfile = "sample.txt"   
 full_file_path_name = os.path.join(mydir, myfile)
 print(full_file_path_name)
 
@@ -32,7 +32,7 @@ def remove_char_from_list(list_of_stuff, char):
 
 def update_seed_map(mapping_info):
 
-    seed_loc_map = {}
+    seed_loc_map = []
 
     for line in mapping_info:
         dest_range_start = int(line.split(' ')[0])
@@ -46,8 +46,12 @@ def update_seed_map(mapping_info):
         update_values = range(dest_range_start,dest_range_start+range_length)
         keys_to_update = range(source_range_start,source_range_start+range_length)
 
-        for index, key in enumerate(keys_to_update):
-            seed_loc_map.update({key:update_values[index]})
+        #for index, key in enumerate(keys_to_update):
+        #    seed_loc_map.update({key:update_values[index]})
+        #
+
+        seed_loc_map.append([dest_range_start,source_range_start,range_length])
+
 
     return seed_loc_map
 
@@ -60,7 +64,7 @@ def main(part):
 
         data = input_file.read().strip().split('\n')
 
-        print(remove_char_from_list(data, ''))
+        remove_char_from_list(data, '')
 
         seeds = remove_char_from_list(data[0].split(':')[1].split(' '), '')
         seed_to_soil = data[data.index("seed-to-soil map:")+1:data.index("soil-to-fertilizer map:")]
@@ -71,14 +75,14 @@ def main(part):
         temp_to_hum = data[data.index("temperature-to-humidity map:")+1:data.index("humidity-to-location map:")]
         hum_to_loc = data[data.index("humidity-to-location map:")+1:]
 
-        print(seeds)
-        print(seed_to_soil)
-        print(soil_fertilizer)
-        print(fertilizer_to_water)
-        print(water_to_light)
-        print(light_to_temp)
-        print(temp_to_hum)
-        print(hum_to_loc)
+        #print(seeds)
+        #print(seed_to_soil)
+        # print(soil_fertilizer)
+        # print(fertilizer_to_water)
+        #print(water_to_light)
+        # print(light_to_temp)
+        # print(temp_to_hum)
+        # print(hum_to_loc)
         
         #initialize some stuff
 
@@ -95,31 +99,66 @@ def main(part):
         maps.append(update_seed_map(temp_to_hum))
         maps.append(update_seed_map(hum_to_loc))
 
-        print(maps)
+        #print(maps)
 
         seed_locations = []
 
 
 
         #iterate through prepared data
-        for line in seeds:
+        for seed_num in seeds:
             #do something for each line
 
-            print("seed # " + str(line))
+            key = int(seed_num)
+
+            #print("seed # " + str(seed_num))
 
             #check if seed is in seed map
+            for i in range(0,7):  ## for each of the 6 mappings 
 
-            # if int(line) in seed_loc_map.keys():
-            #     #then set location to mapped value
-            #     seed_locations.append(seed_loc_map.get(int(line)))
-            #     print("found mapped seed")
 
-            # else:
-            #     seed_locations.append(int(line))
+                found_flag = 0
+                #print(maps[i])
+
+                for conversion in maps[i]:  #check each conversion listed
+
+                    dest_range_start = conversion[0]
+                    source_range_start = conversion[1]
+                    range_length = conversion[2]
+
+                    source_range_end = source_range_start + range_length
+                    dest_range_end = dest_range_start + range_length
+
+                    delta = key - source_range_start
+                    
+                    # print("source range = " + str(source_range_start) + " to " + str(source_range_end))
+                    # print("dest range = " + str(dest_range_start) + " to " + str(dest_range_end))
+                    # print (delta)
+
+
+                    if key >= source_range_start and key <= source_range_end: ## check if seed is in one of the mappings
+                        #print("found mapped key " + str(key) + " in  " + str(maps[i]))
+                        key = dest_range_start + delta
+                        #print("key now equals " + str(key))
+
+                        found_flag = 1
+
+                        break
+
+                if found_flag == 0:
+                        key = key
+                        #print("no key found for " + str(key))
+                else:
+                    found_flag = 1
+
+                
+            
+            seed_locations.append(key)
 
         if part == 1:
    
-            return 1
+            #print(seed_locations)
+            return min(seed_locations)
         else:
 
             return 2
