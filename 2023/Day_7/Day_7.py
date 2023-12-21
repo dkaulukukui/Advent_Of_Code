6c069954678ad6b1affa8f7963c32d6dd02c8c05
Donald Kaulukukui <donald.kaulukukui@arl.hawaii.edu>
2023-12-21 11:39:06 -1000

    Day 7 part 1 in progress

--- 2023/Day_7/Day_7.py
+++ 2023/Day_7/Day_7.py
@@ -3,6 +3,8 @@ from sys import platform
 
 from tqdm import tqdm
 
+from operator import itemgetter  ##to sort list by values
+
 year = 2023
 day = 7
 
@@ -24,6 +26,8 @@ elif platform == "win32":
 myfile = "sample.txt"   
 full_file_path_name = os.path.join(mydir, myfile)
 print(full_file_path_name)
+
+card_order = ['2','3','4','5','6','7','8','9','T','J','Q','K']
 
 def remove_char_from_list(list_of_stuff, char):
 
@@ -50,12 +54,65 @@ def multiplyList(myList):
     for x in myList:
         result = result * x
     return result
- 
+
+
+###return true if hand 1 is better than hand 2
+def compare_hands(hand_1, hand_2):
+    # hands are strings of 5 characters ,"AKQJT98765432", ie "32T3K"
+
+    hand_1_rank = rank_hand(hand_1)
+    hand_2_rank = rank_hand(hand_2)
+
+    if hand_1_rank>hand_2_rank:
+        return True
+    elif hand_1_rank == hand_2_rank:  #hand rankings are equal, check follow on cards
+        for index, card in enumerate(hand_1):
+            if card_order.index(card) > card_order.index(hand_2[index]):  # hand one is higher ranked
+                return True
+            elif card_order.index(card) < card_order.index(hand_2[index]): #hand two is higher ranked
+                return False
+    else:  ## hand two is higher ranked
+        return False
+
+def rank_hand(hand):
+    #rank is a number 1 to 7, 1 = five of a kind, 2 = four of a kind, 3 = full house, 4 = three of a kind, 5 = two pair, 6 = one pair, 7 = high card
+
+    for char in hand:
+        if hand.count(char) == 5: #five of a kind
+            return 1
+        if hand.count(char) == 4: #four of a kind
+            return 2
+        if hand.count(char) == 3: # 3 of a kind, check for full house
+            #check for full house
+            temp_hand=remove_char_from_list(hand,char) #remove the three of a kind from the list
+            #if full house return 3
+            if temp_hand[0] == temp_hand[1]: #remaining cards is a pair
+                return 3
+            else:
+                return 4 #remining cards dont match so not a full house
+        if hand.count(char) == 2: #found a pair, check for full house and two pair
+            temp_hand=remove_char_from_list(hand,char) #remove the pair from the list
+            if temp_hand.count(temp_hand[0]) == 3: #fullhouse
+                return 3
+            elif temp_hand.count(temp_hand[0]) == 2: #another pair, so two pair
+                return 5
+            elif temp_hand.count(temp_hand[1]) == 2: #another pair, so two pair
+                return 5
+            else: #no more pairs, return single pair
+                return 6
+    #nothing found so return high card
+
+    return 7 
+
+
+
+    return rank
+
 def main(part):
 
 
     data = read_in_data_file(full_file_path_name)
-    print(data)
+    #print(data)
 
     hands = []
 
@@ -63,8 +120,21 @@ def main(part):
         hand = hand.split(' ')
         hands.append(hand)
 
+
+    for hand in hands:
+        hand[0] = list(hand[0])
+        hand[1] = int(hand[1])
+        hand.append(rank_hand(hand[0]))  #add ranking metric to hands
+
+    #sort hand
+
+
     print(hands)
+
+    print(sorted(hands, key=itemgetter(21)))
     #initialize some stuff
+
+        # build ranked list of hands
 
 
     if part == 1:
