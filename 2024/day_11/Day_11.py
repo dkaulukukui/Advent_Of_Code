58c8627c43d8aa32dbe0afc1654ecbedf1590fca
Donald Kaulukukui <you@example.com>
2024-12-18 12:19:48 -1000

    Just say no to brute force!

--- 2024/day_11/Day_11.py
+++ 2024/day_11/Day_11.py
@@ -3,7 +3,7 @@ from input_processing import get_full_filepath
 from AOC_DEBUG import debug_print
 
 part_1_debug = False
-part_2_debug = False
+part_2_debug = True
 
 year = 2024
 day = 11
@@ -22,6 +22,32 @@ def day11_parse_data(full_file_path_name):
         data = input_file.read().strip().split(' ')
         
     return data
+
+def day11_collapse_list(input_list):
+
+    #given a list, return a list of tuples with each item in the list and the number of occurrences
+    # occurrence_dict = {}
+    results = []
+    result_count = []
+
+    for i in range(0, len(input_list)):
+        item = input_list[i][0]
+        count = input_list[i][1]
+
+        if(item in results):
+            result_count[results.index(item)] += count
+        else:
+            results.append(item)
+            result_count.append(count)
+
+    result_tuple = []
+
+    for i in range(0, len(results)):
+
+        result_tuple.append((results[i], result_count[i]))
+
+    return result_tuple
+
 
 def day11_calculate_num_rocks(num, iterations):
      
@@ -54,8 +80,58 @@ def day11_calculate_num_rocks(num, iterations):
                 modified_rocks.append(int(rock)*2024)
         
         rocks = modified_rocks
+        # print("1: iteration # " + str(i+1))
+        # print(rocks)
+    
 
     return len(rocks)
+
+def day11_part2_calculate_num_rocks(num, iterations):
+     
+    rocks = [(num,1)]
+    rocks_collapsed = day11_collapse_list(rocks)
+     
+    for i in range(0,iterations):
+
+        modified_rocks = []
+
+        for index in range(0, len(rocks_collapsed)):
+            rock = rocks_collapsed[index][0]
+            count = rocks_collapsed[index][1]
+
+            # If the stone is engraved with the number 0, it is replaced by a stone engraved with the number 1.
+            if rock == 0:
+                modified_rocks.append((1,count))
+            # If the stone is engraved with a number that has an even number of digits, it is replaced by two stones. 
+            # The left half of the digits are engraved on the new left stone, and the right half of the digits are engraved on the new right stone. 
+            # (The new numbers don't keep extra leading zeroes: 1000 would become stones 10 and 0.)
+            elif len(str(rock))%2 == 0:
+                rock_string = str(rock)
+                rock_length = len(rock_string)
+                dividing_point = int(rock_length/2)
+                first_rock = rock_string[:dividing_point]
+                second_rock = rock_string[dividing_point:]
+
+                modified_rocks.append((int(first_rock), count))
+                modified_rocks.append((int(second_rock), count))
+            # If none of the other rules apply, the stone is replaced by a new stone; the old stone's number multiplied by 2024 is engraved on the new stone.
+            else:
+                modified_rocks.append((int(rock)*2024, count))
+            
+        # print("2: iteration # " + str(i+1))
+        # print(modified_rocks)
+    
+        #
+        #debug_print(modified_rocks,part_2_debug)
+
+        rocks_collapsed = day11_collapse_list(modified_rocks)
+
+    # debug_print(rocks_collapsed,part_2_debug)
+
+    rock_count =  sum(item[1] for item in rocks_collapsed)
+
+    return rock_count
+
 
 
 def main(part):
@@ -146,7 +222,15 @@ def main(part):
             start_time = time.time()
 
             for rock in rocks:
-                part_2_answer += day11_calculate_num_rocks(int(rock), 75)
+                rocks_to_add = day11_part2_calculate_num_rocks(int(rock),75)
+                part_2_answer += rocks_to_add
+                # print(f"Num of Rocks: {rocks_to_add}")
+
+                
+                # part_1 = day11_calculate_num_rocks(int(rock), 25)
+
+                # print(f"Correct Num of Rocks: {part_1}")
+
 
             end_time = time.time()
 
